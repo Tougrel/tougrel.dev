7703202c1a2793ef3154691f7bf1a8a9324f427b
Tougrel <lambrosagris@gmail.com>
2024-10-14 17:14:34 +0300

    refactor(nuxt): replace old modules and settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	compatibilityDate: "2024-10-14",
+
 	nitro: {
 		preset: process.env.NITRO_PRESET || "node",
 	},
 
-	modules: [
-		"@nuxtjs/fontaine",
-		"@nuxtjs/google-fonts",
-		"@nuxt/devtools",
-		"@nuxt/ui",
-		"nuxt-cloudflare-analytics",
-		"nuxt-icon",
-	],
+	modules: ["@nuxt/fonts", "@nuxt/devtools", "@nuxt/ui", "@nuxt/icon", "nuxt-cloudflare-analytics"],
 
 	runtimeConfig: {
 		public: {},
 	},
 
-	googleFonts: {
-		preload: true,
-		inject: true,
-		download: false,
-		prefetch: true,
-		preconnect: true,
-		base64: true,
-		families: {
-			Poppins: {
-				wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-				ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-			},
-			// "Noto+Emoji": {
-			// 	wght: [300, 400, 500, 600, 700],
-			// },
+	fonts: {
+		provider: "bunny",
+		defaults: {
+			weights: [400, 500, 600, 700, 800],
 		},
-	},
-
-	ui: {
-		icons: ["heroicons", "mdi"],
-	},
-
-	fontMetrics: {
-		fonts: ["Poppins", "Ubuntu"],
+		experimental: {
+			processCSSVariables: true,
+		},
 	},
 
 	cloudflareAnalytics: {
